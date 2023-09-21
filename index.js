aa20b0086d82715c75217fe50d72c74dd56c3745
Alex Isaacs <alexisaacs18@gmail.com>
2023-09-21 13:05:32 -0400

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+    let messages = [];
+    names.forEach((name) => {
+      messages.push(`Thank you, ${name}, for the wonderful ${gift} gift!`);
+    });
+    return messages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
